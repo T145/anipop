6ed6c555e304a707ab598e0d5ac5f7fd07f5715f
T145 <T145@protonmail.com>
2019-10-19 02:22:45 -0700

    Added more profile prefs & optimized addon install

--- anipop.py
+++ anipop.py
@@ -20,41 +20,58 @@ QB = Client('http://127.0.0.1:8080/')
 
 
 def get_browser():
-    os.mkdir(ADDONS_PATH)
+    if not os.path.exists(ADDONS_PATH):
+        os.mkdir(ADDONS_PATH)
+
     ext_prefix = 'https://addons.mozilla.org/en-US/firefox/addon/'
     exts = [
         'ublock-origin',  # Blocks ads & such
-        'umatrix',  # Will block Disqus on HorribleSubs automatically
         'https-everywhere',  # TODO: Figure out how to enable 'Encryt All Sites Eligble'
         'decentraleyes',  # Blocks Content Management Systems and handles their abilities locally
+        'umatrix'  # Will block Disqus on HorribleSubs automatically
     ]
     profile = webdriver.FirefoxProfile()
 
     # Download settings (in case a more efficient means of downloading XPIs through the browser is discovered)
     profile.set_preference('browser.download.folderList', 2)
     profile.set_preference('browser.download.manager.showWhenStarting', False)
-    profile.set_preference('browser.download.dir', ADDONS_PATH)
-    profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/x-gzip')
+    #profile.set_preference('browser.download.dir', ADDONS_PATH)
+    #profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/x-gzip')
 
-    # Privacy settings (https://restoreprivacy.com/firefox-privacy/)
-    # TODO: Cross-reference w/ https://www.privacytools.io/
+    # Run the browser in private mode
     profile.set_preference('extensions.allowPrivateBrowsingByDefault', True)
     profile.set_preference('browser.privatebrowsing.autostart', True)
-    profile.set_preference('media.peerconnection.enabled', False)
+
+    # Privacy settings (https://www.privacytools.io/)
+    profile.set_preference('media.peerconnection.turn.disable', True)
+    profile.set_preference('media.peerconnection.use_document_iceservers', False)
+    profile.set_preference('media.peerconnection.video.enabled', False)
+    profile.set_preference('media.peerconnection.identity.timeout', 1)
+    profile.set_preference('privacy.firstparty.isolate', True)
     profile.set_preference('privacy.resistFingerprinting', True)
     profile.set_preference('privacy.trackingprotection.fingerprinting.enabled', True)
     profile.set_preference('privacy.trackingprotection.cryptomining.enabled', True)
-    profile.set_preference('privacy.firstparty.isolate', True)
     profile.set_preference('privacy.trackingprotection.enabled', True)
-    profile.set_preference('geo.enabled', False)
+    profile.set_preference('browser.send_pings', False)
+    profile.set_preference('browser.sessionstore.max_tabs_undo', 0)
+    profile.set_preference('browser.sessionstore.privacy_level', 2)
+    profile.set_preference('browser.urlbar.speculativeConnect.enabled', False)
+    profile.set_preference('dom.event.clipboardevents.enabled', False)
+    profile.set_preference('media.eme.enabled', False)
+    profile.set_preference('media.gmp-widevinecdm.enabled', False)
     profile.set_preference('media.navigator.enabled', False)
     profile.set_preference('network.cookie.cookieBehavior', 2)
     profile.set_preference('network.cookie.lifetimePolicy', 2)
+    profile.set_preference('network.http.referer.XOriginPolicy', 2)
+    profile.set_preference('network.http.referer.XOriginTrimmingPolicy', 2)
+    profile.set_preference('network.IDN_show_punycode', True)
+    profile.set_preference('webgl.disabled', True)
+
+    # Settings unique to https://restoreprivacy.com/firefox-privacy/
+    profile.set_preference('geo.enabled', False)
+    profile.set_preference('media.peerconnection.enabled', False)
     profile.set_preference('network.dns.disablePrefetch', True)
     profile.set_preference('network.prefetch-next', False)
-    profile.set_preference('webgl.disabled', True)
-    profile.set_preference('dom.event.clipboardevents.enabled', False)
-    profile.set_preference('media.eme.enabled', False)
 
     browser = webdriver.Firefox(firefox_profile=profile)
 
@@ -63,12 +80,8 @@ def get_browser():
         btn = browser.find_element_by_class_name('AMInstallButton')
         ref = btn.find_element_by_tag_name('a').get_attribute('href')
         url = ref.split('?')[0]
-        wget.download(url, out=ADDONS_PATH)
-
-    for cwd, _, addons in os.walk(ADDONS_PATH):
-        for addon in addons:
-            addon_path = os.path.join(cwd, addon)
-            browser.install_addon(addon_path, temporary=True)
+        addon = wget.download(url, out=ADDONS_PATH).replace('/', '')
+        browser.install_addon(addon, temporary=True)
 
     # Just to escape out of wget's printed progress bar to normalize future logging
     print('\n')
@@ -88,20 +101,20 @@ if __name__ == "__main__":
     parser = Soup(src, features='html.parser')
     divs = parser.body.find_all('div', attrs={'class': 'ind-show'})
     size = len(divs)
-    print('Downloading ', size, ' shows')
+    print('Downloading', size, 'shows')
 
     for i, div in enumerate(divs):
         browser.get('https://horriblesubs.info' + div.a['href'])
 
         # Wait to dodge `selenium.common.exceptions.ElementNotInteractableException: Message: Element could not be scrolled into view`
-        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'more-button')))
+        WebDriverWait(browser, 15).until(EC.element_to_be_clickable((By.CLASS_NAME, 'more-button')))
 
         # Expand the whole listing to get all the episodes
         if not browser.find_elements_by_id('01'):
             try:
                 while True:
                     browser.find_element_by_class_name('more-button').click()
-            except NoSuchElementException as err:
+            except NoSuchElementException:
                 pass
 
         src = browser.page_source
@@ -123,6 +136,6 @@ if __name__ == "__main__":
                 magnet = link_rel.find('a', attrs={'title': 'Magnet Link'})['href']
                 QB.download_from_link(magnet, category='anime', savepath=DL_PATH + div.a.text)
 
-        print('Progress: ', round(((i + 1) / size) * 100, 2), '%')
+        print('Progress:', round(((i + 1) / size) * 100, 2), '%')
 
     quit(browser)
